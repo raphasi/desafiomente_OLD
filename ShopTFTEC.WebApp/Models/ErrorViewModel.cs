54ee16f0bf20d963f5c16199a49bc28e6045a589
raphael@tftec.com.br <raphael@tftec.com.br>
2023-11-14 12:39:40 -0300

    Revert "Ajustes da Área de aplicação do cliente."
    This reverts commit 06dd1b41e6acd17199713c21d4b9e546929fc1e0.

--- ShopTFTEC.WebApp/Models/ErrorViewModel.cs
+++ ShopTFTEC.WebApp/Models/ErrorViewModel.cs
@@ -1,9 +1,8 @@
-namespace ShopTFTEC.WebApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+namespace ShopTFTEC.WebApp.Models;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
